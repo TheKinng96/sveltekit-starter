19a1077ded2c6044ac128a77fe76d2afbc252253
G3N <fengyuan.yap@gmail.com>
2024-10-13 15:13:18 +0900

    added tailwind css auto formatter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,38 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { createCollapsible, melt } from '@melt-ui/svelte';
-	import { AppButton } from '$lib/components/base/button';
-	import { fade } from 'svelte/transition';
+	import { onMount } from 'svelte'
+	import { createCollapsible, melt } from '@melt-ui/svelte'
+	import { AppButton } from '$lib/components/base/button'
+	import { fade } from 'svelte/transition'
 
-	let darkMode = false;
+	let darkMode = false
 
 	function toggleTheme() {
-		darkMode = !darkMode;
-		document.documentElement.setAttribute('data-theme', darkMode ? 'dark' : 'light');
+		darkMode = !darkMode
+		document.documentElement.setAttribute('data-theme', darkMode ? 'dark' : 'light')
 	}
 
 	onMount(() => {
-		darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-		document.documentElement.setAttribute('data-theme', darkMode ? 'dark' : 'light');
-	});
+		darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
+		document.documentElement.setAttribute('data-theme', darkMode ? 'dark' : 'light')
+	})
 
 	const {
 		elements: { root, content, trigger },
-		states: { open }
+		states: { open },
 	} = createCollapsible({
-		forceVisible: true
-	});
+		forceVisible: true,
+	})
 </script>
 
 <button
 	on:click={toggleTheme}
 	type="button"
-	class=" shadow-mini hover:bg-dark/95 active:scale-98 active:transition-all p-2 m-4 bg-light-secondary-default dark:bg-dark-secondary-default text-light-text-default rounded"
+	class="shadow-mini hover:bg-dark/95 active:scale-98 m-4 rounded bg-light-secondary-default p-2 text-light-text-default active:transition-all dark:bg-dark-secondary-default"
 >
 	{darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
 </button>
 
-<h1 class="underline text-2xl font-bold text-light-secondary">Welcome to SvelteKit</h1>
+<h1 class="text-light-secondary text-2xl font-bold underline">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <div class="root" use:melt={$root}>
@@ -42,7 +42,7 @@
 
 	{#if $open}
 		<div
-			class="rounded-md border border-light-border-default dark:border-dark-border-default p-4 text-light-primary-dark dark:text-light-primary-light shadow-sm"
+			class="rounded-md border border-light-border-default p-4 text-light-primary-dark shadow-sm dark:border-dark-border-default dark:text-light-primary-light"
 			use:melt={$content}
 			transition:fade
 		>
